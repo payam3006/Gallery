b1d822dc6ca22e2d1619c3a52328a9dff1ed0b48
Payam Mohajerin Esfahani <payam3006@gmail.com>
2023-02-03 00:07:42 +0330

    add select a thumb and open it with some better css

--- gallery.js
+++ gallery.js
@@ -1,52 +1,87 @@
-//<img class="thumbs" src="img/thumbs2/1.jpg" alt=""></img>
+let openedImg = 0;
+let selectedImg = 0;
+
+//<img class="thumbsContainer" src="img/thumbs2/1.jpg" alt=""></img>
 const removeThumbs = () => {
-  document.getElementById("thumbs").innerHTML = "";
+  document.getElementById("thumbsContainer").innerHTML = "";
   //   document
-  //     .getElementById("thumbs")
+  //     .getElementById("thumbsContainer")
   //     .removeChild(document.getElementsByClassName("imageContainer"));
+};
+
+const selectThumb = (i) => {
+  if (selectedImg !== i) {
+    if (selectedImg !== 0) {
+      document
+        .getElementById(`imgContainer${selectedImg}`)
+        .classList.remove("selected");
+    }
+    document.getElementById(`imgContainer${i}`).classList.add("selected");
+    selectedImg = i;
+  }
+};
+
+const openImage = (i) => {
+  document
+    .getElementById("biggerImageBackground")
+    .setAttribute("style", "display:block");
+  document
+    .getElementById("biggerImageBackground")
+    .classList.add("biggerImageBackground");
+  document.getElementById("bigImg").setAttribute("src", `img/images/${i}.jpg`);
+  document.getElementById("bigImg").setAttribute("style", "display:block");
+  document.getElementById("thumbsContainer").classList.add("thumbsContainer2");
+  document.getElementById("body").classList.add("body2");
+  document.getElementById("bigImg").classList.add("bigImg");
 };
 
 const fillWithBigThumbs = () => {
   removeThumbs();
-  for (i = 1; i <= 40; i++) {
+  for (let i = 1; i <= 40; i++) {
     let imageContainer = document.createElement("div");
     imageContainer.className = "imgContainer";
     imageContainer.id = `imgContainer${i}`;
-    document.getElementById("thumbs").appendChild(imageContainer);
+    imageContainer.setAttribute("onclick", `selectThumb(${i})`);
+    imageContainer.setAttribute("ondblclick", `openImage(${i})`);
+
+    document.getElementById("thumbsContainer").appendChild(imageContainer);
 
     let image = document.createElement("img");
     image.src = `img/thumbs1/${i}.jpg`;
-    image.className = "imgthumb";
+    image.className = "imgThumb";
     document.getElementById(`imgContainer${i}`).appendChild(image);
   }
 };
 
 const fillWithSmallThumbs = () => {
   removeThumbs();
-  for (i = 1; i <= 40; i++) {
+  for (let i = 1; i <= 40; i++) {
     let imageContainer = document.createElement("div");
     imageContainer.className = "imgContainer";
     imageContainer.id = `imgContainer${i}`;
-    document.getElementById("thumbs").appendChild(imageContainer);
+    imageContainer.setAttribute("onclick", `selectThumb(${i})`);
+    imageContainer.setAttribute("ondblclick", `openImage(${i})`);
+
+    document.getElementById("thumbsContainer").appendChild(imageContainer);
 
     let image = document.createElement("img");
     image.src = `img/thumbs2/${i}.jpg`;
-    image.className = "imgthumb";
+    image.className = "imgThumb";
     document.getElementById(`imgContainer${i}`).appendChild(image);
   }
 };
 
 const bodyWidth = document.getElementById("body").offsetWidth;
-if (bodyWidth >= 600) {
+if (bodyWidth >= 700) {
   fillWithBigThumbs();
 } else {
   fillWithSmallThumbs();
 }
 
 const resize = () => {
-  console.log("now!");
+  // console.log("now!");
   let bodyWidth = document.getElementById("body").offsetWidth;
-  if (bodyWidth >= 600) {
+  if (bodyWidth >= 700) {
     fillWithBigThumbs();
   } else {
     fillWithSmallThumbs();
