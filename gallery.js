5052336cb65181973f1b302bfeac407e9ad4b46b
Payam Mohajerin Esfahani <payam3006@gmail.com>
2023-02-12 04:01:25 +0330

    add mouse scroll up and down effect on opened image to go next or previous

--- gallery.js
+++ gallery.js
@@ -22,42 +22,50 @@ const selectThumb = (i) => {
 };
 
 const openImage = (i) => {
-  document
-    .getElementById("biggerImageBackground")
-    .setAttribute("style", "display:block");
+  if (i !== 0) {
+    document
+      .getElementById("biggerImageBackground")
+      .setAttribute("style", "display:block");
 
-  document.getElementById("bigImg").setAttribute("src", `img/images/${i}.jpg`);
-  document
-    .getElementById("biggerImgFrame")
-    .setAttribute("style", "display:block");
-  document.getElementById("thumbsContainer").classList.add("thumbsContainer2");
-  document.getElementById("body").classList.add("body2");
-  openedImg = i;
-  checkButtonsAvailability();
+    document
+      .getElementById("bigImg")
+      .setAttribute("src", `img/images/${i}.jpg`);
+    document
+      .getElementById("biggerImgFrame")
+      .setAttribute("style", "display:block");
+    document
+      .getElementById("thumbsContainer")
+      .classList.add("thumbsContainer2");
+    document.getElementById("body").classList.add("body2");
+    openedImg = i;
+    checkButtonsAvailability();
+  }
 };
 
 const closeImage = () => {
-  document
-    .getElementById("biggerImageBackground")
-    .setAttribute("style", "display:none");
-  document
-    .getElementById("biggerImgFrame")
-    .classList.add("biggerImgFrameClosing");
-  setTimeout(function close() {
+  if (openedImg !== 0) {
     document
-      .getElementById("biggerImgFrame")
+      .getElementById("biggerImageBackground")
       .setAttribute("style", "display:none");
-  }, 1000);
-  setTimeout(function close2() {
     document
       .getElementById("biggerImgFrame")
-      .classList.remove("biggerImgFrameClosing");
-  }, 1000);
-  document
-    .getElementById("thumbsContainer")
-    .classList.remove("thumbsContainer2");
-  document.getElementById("body").classList.remove("body2");
-  openedImg = 0;
+      .classList.add("biggerImgFrameClosing");
+    setTimeout(function close() {
+      document
+        .getElementById("biggerImgFrame")
+        .setAttribute("style", "display:none");
+    }, 1000);
+    setTimeout(function close2() {
+      document
+        .getElementById("biggerImgFrame")
+        .classList.remove("biggerImgFrameClosing");
+    }, 1000);
+    document
+      .getElementById("thumbsContainer")
+      .classList.remove("thumbsContainer2");
+    document.getElementById("body").classList.remove("body2");
+    openedImg = 0;
+  }
 };
 
 /////////////////close BigImg///////////////////////
@@ -153,6 +161,7 @@ if (bodyWidth >= 700) {
   fillWithSmallThumbs();
 }
 
+////////////////////////////Maybe Not Usable!!!//////////////////////
 const resize = () => {
   // console.log("now!");
   let bodyWidth = document.getElementById("body").offsetWidth;
@@ -164,3 +173,38 @@ const resize = () => {
 };
 
 window.addEventListener("resize", resize);
+////////////////////////////Maybe Not Usable!!!//////////////////////
+
+////////////////////keyboard acts////////////////////////////////
+const keyboardAction = (event) => {
+  let character = event.key;
+  if (character == "ArrowRight" && openedImg !== 0) {
+    goNext();
+  }
+  if (character == "ArrowLeft" && openedImg !== 0) {
+    goPrevious();
+  }
+  if (character == "Enter" && openedImg == 0) {
+    openImage(selectedImg);
+  }
+  if (character == "Escape") {
+    closeImage();
+  }
+  // console.log(character);
+};
+
+document.body.addEventListener("keydown", keyboardAction);
+
+////////////////////mouse acts////////////////////////////////
+const mouseAction = (A) => {
+  let scroll = A.deltaY;
+  if (scroll == -100 && openedImg !== 0) {
+    goNext();
+  }
+  if (scroll == 100 && openedImg !== 0) {
+    goPrevious();
+  }
+};
+
+const frame = document.getElementById("biggerImgFrame");
+frame.addEventListener("wheel", mouseAction);
